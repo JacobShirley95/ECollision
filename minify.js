bc9663caad6aaa40f2ecd5a9913280494ab910d9
Jacob Shirley <auriferous95@gmail.com>
2016-02-01 15:46:59 +0000

    Made the minifier compile all dependencies (except jquery) into one file.

--- minify.js
+++ minify.js
@@ -22,8 +22,8 @@ new compressor.minify({
 
 new compressor.minify({
   type: 'no-compress',
-  fileIn: ['bower_components/EaselJS/lib/easeljs-0.8.2.min.js', 'bin/ecollision.min.js'],
-  fileOut: 'bin/ecollision.easeljs.min.js',
+  fileIn: ['bower_components/EaselJS/lib/easeljs-0.8.2.min.js', 'bower_components/jquery-mousewheel/jquery.mousewheel.min.js', 'bin/ecollision.min.js'],
+  fileOut: 'bin/ecollision.deps.min.js',
   callback: function(err, min){
   	if (err != null)
   		console.log(err);
