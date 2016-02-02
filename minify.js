b05ba6ac99b1198d52bf46c3581cbd98bae92265
Jacob Shirley <auriferous95@gmail.com>
2016-02-02 17:33:00 +0000

    Renamed ecollision.deps.min.js to ecollision.standalone.min.js.

--- minify.js
+++ minify.js
@@ -23,7 +23,7 @@ new compressor.minify({
 new compressor.minify({
   type: 'no-compress',
   fileIn: ['bower_components/EaselJS/lib/easeljs-0.8.2.min.js', 'bower_components/jquery-mousewheel/jquery.mousewheel.min.js', 'bin/ecollision.min.js'],
-  fileOut: 'bin/ecollision.deps.min.js',
+  fileOut: 'bin/ecollision.standalone.min.js',
   callback: function(err, min){
   	if (err != null)
   		console.log(err);
